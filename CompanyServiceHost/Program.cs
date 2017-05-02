2b7416fd2268f9d2b91569e00267857bffe99eed
wanasak <u510610433@gmail.com>
2017-05-02 16:41:54 +0700

    wcf service implementing multiple service constracts

---
+++ CompanyServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CompanyService.CompanyService)))
+            {
+                host.Open();
+
+                Console.WriteLine($"Host started @{DateTime.Now.ToString()}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
