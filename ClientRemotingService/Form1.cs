11cc8abb4cd942153ef177f5b34ddb9b8ce2308b
wanasak <u510610433@gmail.com>
2017-05-02 14:39:33 +0700

    creating a remoting service and a web service

---
+++ ClientRemotingService/Form1.cs
@@ -0,0 +1,35 @@
+﻿using IRemotingService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientRemotingService
+{
+    public partial class Form1 : Form
+    {
+        IHelloRemotingService client;
+        public Form1()
+        {
+            InitializeComponent();
+
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService)Activator.GetObject(
+              typeof(IHelloRemotingService),
+              "tcp://localhost:8080/GetMessage");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = client.GetMessage(textBox1.Text);
+        }
+    }
+}
---
+++ RemotingServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using IRemotingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloRemotingService remotingService = new HelloRemotingService();
+            TcpChannel channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+               typeof(HelloRemotingService), "GetMessage",
+               WellKnownObjectMode.Singleton);
+
+
+            Console.WriteLine($"Host started @{DateTime.Now.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
