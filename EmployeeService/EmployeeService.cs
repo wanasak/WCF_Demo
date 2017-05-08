f195c7d7c2801900bc33f715cc5eb406faf6145a
wanasak <u510610433@gmail.com>
2017-05-08 15:18:38 +0700

    risk of implementing extensiondataobject - DoS attack, the extension is store in memory, the attacker may foold the server with request that contain many unknow elements

--- EmployeeService/EmployeeService.cs
+++ EmployeeService/EmployeeService.cs
@@ -12,6 +12,8 @@ namespace EmployeeService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in both code and config file together.
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    // Disable ExtensionDataObject
+    //[ServiceBehavior(IgnoreExtensionDataObject = true)]
     public class EmployeeService : IEmployeeService
     {
         private Employee _lastEmployee;
