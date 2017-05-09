f15605bfe5efc7f5a82230e99c8bbe28d268161e
wanasak <u510610433@gmail.com>
2017-05-09 08:51:54 +0700

    unhandled exceptions wcf

---
+++ CalculatorWinClient/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CalculatorWinClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumerator = new System.Windows.Forms.TextBox();
+            this.txtDenominator = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numerator";
+            // 
+            // txtNumerator
+            // 
+            this.txtNumerator.Location = new System.Drawing.Point(99, 29);
+            this.txtNumerator.Name = "txtNumerator";
+            this.txtNumerator.Size = new System.Drawing.Size(147, 20);
+            this.txtNumerator.TabIndex = 1;
+            // 
+            // txtDenominator
+            // 
+            this.txtDenominator.Location = new System.Drawing.Point(99, 55);
+            this.txtDenominator.Name = "txtDenominator";
+            this.txtDenominator.Size = new System.Drawing.Size(147, 20);
+            this.txtDenominator.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Denominator";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Divide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(29, 113);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(217, 48);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Create New Proxy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDenominator);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumerator);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumerator;
+        private System.Windows.Forms.TextBox txtDenominator;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CalculatorWinClient/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// An unhandled exceptions in wcf service, will cause the communication channel to fault and the session will be lost
+// We cannot use the same instance of the proxy class anymore.
+// BasicHttpBinding - When there is an unhandled exception, it only fault the server channel. The client proxy is still OK. Because BasicHttpBinding the channel is not maintaining sessions.
+// wsHttpBinding - When there is an unhandled exception, it fault the server channel and client proxy also. Because wsHttpBinding the channel is maintaining secure session.
+
+namespace CalculatorWinClient
+{
+    public partial class Form1 : Form
+    {
+        CalculatorService.CalculatorServiceClient _client;
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new CalculatorService.CalculatorServiceClient();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numerator = Convert.ToInt32(txtNumerator.Text);
+                int denominator = Convert.ToInt32(txtDenominator.Text);
+                lblStatus.Text = _client.Devide(numerator, denominator).ToString();
+            }
+            catch (FaultException ex)
+            {
+                lblStatus.Text = ex.Message.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _client = new CalculatorService.CalculatorServiceClient();
+        }
+    }
+}
